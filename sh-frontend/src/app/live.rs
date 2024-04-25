ee98a7889f4b2110f3eadd69bec50c31acb6e12c
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-04-25 11:41:33 -0600

    wip

--- sh-frontend/src/app/live.rs
+++ sh-frontend/src/app/live.rs
@@ -4,13 +4,23 @@
 // https://opensource.org/licenses/MIT.
 
 use yew::prelude::*;
+use yew_hooks::use_event_with_window;
 
 /// Home page
 #[function_component(Live)]
 pub fn live() -> Html {
+    use_event_with_window("keydown", move |e: KeyboardEvent| {
+        log::debug!("Key pressed: {}", e.key());
+    });
+
     html! {
-        {
-            "Placeholder"
-        }
+        <div class="content flex width-max height-max">
+            <div class="content flex h-max max-w-96 ">
+                {"ACARS Box"}
+             </div>
+            <div class="content flex pl-2">
+                {"Map Box"}
+            </div>
+        </div>
     }
 }
