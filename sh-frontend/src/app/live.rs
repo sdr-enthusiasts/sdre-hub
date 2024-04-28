55b6c2c091047153992807c9fde644ad186a6e97
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-04-28 05:50:21 -0600

    use keys to change panels

--- sh-frontend/src/app/live.rs
+++ sh-frontend/src/app/live.rs
@@ -8,20 +8,150 @@ use crate::components::map_display::ShMap;
 use yew::prelude::*;
 use yew_hooks::use_event_with_window;
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+enum Panels {
+    Messages,
+    Map,
+    Settings,
+    Help,
+    None,
+}
+
+impl Panels {
+    fn next(&self, skip: Panels) -> Panels {
+        // go to the next panel, skipping the one we're currently on
+
+        match self {
+            Panels::Messages => {
+                if skip == Panels::Map {
+                    Panels::Settings
+                } else {
+                    Panels::Map
+                }
+            }
+            Panels::Map => {
+                if skip == Panels::Settings {
+                    Panels::Help
+                } else {
+                    Panels::Settings
+                }
+            }
+            Panels::Settings => {
+                if skip == Panels::Help {
+                    Panels::Messages
+                } else {
+                    Panels::Help
+                }
+            }
+            Panels::Help => {
+                if skip == Panels::Messages {
+                    Panels::Map
+                } else {
+                    Panels::Messages
+                }
+            }
+            Panels::None => Panels::None,
+        }
+    }
+
+    fn previous(&self, skip: Panels) -> Panels {
+        // go to the previous panel, skipping the one we're currently on
+
+        match self {
+            Panels::Messages => {
+                if skip == Panels::Help {
+                    Panels::Settings
+                } else {
+                    Panels::Help
+                }
+            }
+            Panels::Map => {
+                if skip == Panels::Messages {
+                    Panels::Help
+                } else {
+                    Panels::Messages
+                }
+            }
+            Panels::Settings => {
+                if skip == Panels::Map {
+                    Panels::Messages
+                } else {
+                    Panels::Map
+                }
+            }
+            Panels::Help => {
+                if skip == Panels::Settings {
+                    Panels::Map
+                } else {
+                    Panels::Settings
+                }
+            }
+            Panels::None => Panels::None,
+        }
+    }
+}
+
 /// Home page
 #[function_component(Live)]
 pub fn live() -> Html {
+    // TODO: Grab current state from local storage
+    let left_panel = use_state(|| Panels::Messages);
+    let right_panel = use_state(|| Panels::Map);
+
+    let right_panel_status = {
+        match *right_panel {
+            Panels::Messages => html! { <AcarsMessages /> },
+            Panels::Map => html! { <ShMap /> },
+            Panels::Settings => html! { <div>{"Settings"}</div> },
+            Panels::Help => html! { <div>{"Help"}</div> },
+            Panels::None => html! { <div>{"None"}</div> },
+        }
+    };
+
+    let left_panel_show = {
+        match *left_panel {
+            Panels::Messages => html! { <AcarsMessages /> },
+            Panels::Map => html! { <ShMap /> },
+            Panels::Settings => html! { <div>{"Settings"}</div> },
+            Panels::Help => html! { <div>{"Help"}</div> },
+            Panels::None => html! { <div>{"None"}</div> },
+        }
+    };
+
     use_event_with_window("keydown", move |e: KeyboardEvent| {
         log::debug!("Key pressed: {}", e.key());
+
+        let right_panel = right_panel.clone();
+        let left_panel = left_panel.clone();
+
+        // if control is pressed, with left arrow, go to the previous panel
+        if e.shift_key() && e.key() == "ArrowLeft" {
+            right_panel.set(right_panel.previous(*left_panel));
+        }
+
+        // if control is pressed, with right arrow, go to the next panel
+        if e.shift_key() && e.key() == "ArrowRight" {
+            right_panel.set(right_panel.next(*left_panel));
+        }
+
+        // if alt is pressed, with left arrow, go to the previous panel
+        if e.alt_key() && e.key() == "ArrowLeft" {
+            left_panel.set(left_panel.previous(*right_panel));
+        }
+
+        // if alt is pressed, with right arrow, go to the next panel
+        if e.alt_key() && e.key() == "ArrowRight" {
+            left_panel.set(left_panel.next(*right_panel));
+        }
     });
 
     html! {
         <div class="content flex w-full h-screen">
             <div class="content p-2 m-0 mt-1 md:w-96 h-full w-full rounded-2xl border-[#8963ba] border-4" id="live-left">
-               <AcarsMessages />
+                { left_panel_show.clone() }
              </div>
             <div class="content m-0 mt-1 ml-2 h-full w-full rounded-2xl border-[#8963ba] border-4 hidden md:block" style="overflow:hidden" id="live-right">
-                <ShMap />
+                { right_panel_status.clone() }
             </div>
         </div>
     }
